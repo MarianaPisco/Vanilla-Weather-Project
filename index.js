c98dcea0cdb94fe2358f5be2ca4b1960bb0f2803
MarianaPisco <98669853+MarianaPisco@users.noreply.github.com>
2022-03-24 13:55:23 +0000

    api integration

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-javascript code here
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0${hours}";
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = "0${minutes}";
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return "${day} ${hours}:${minutes}";
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let city = "Paris";
+let apiKey = "b01b330012699bdf6ed530375ada87c3";
+let url =
+  "http://api.openweathermap.org/data/2.5/weather?p=${city}&appid=${apiKey}&units=metric";
+
+axios.get(url).then(displayTemperature);
